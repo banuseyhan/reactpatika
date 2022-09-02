ea31d916d6b7bc83bdf4911cacfc25e576075f3c
Banu Seyhan <banuseyhan@Banu-MacBook-Pro.local>
2022-09-02 10:54:04 +0300

    done

---
+++ app.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export default async (userId) => {
+    const user = await (await axios(`https://jsonplaceholder.typicode.com/users/${userId}`)).data;
+    const posts = await (await axios(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)).data;
+
+
+    return {
+        user,
+        posts
+    }
+}
---
+++ index.js
@@ -0,0 +1,6 @@
+import getData from './app.js';
+
+await getData(1)
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+ 
